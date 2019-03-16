cddf1a765c386d0740b942ca1e3a69f293025399
Suilong Luo <suilong24@gmail.com>
2019-03-16 19:26:43 -0400

    directory fixup

---
+++ cruising-app/cruising-app/Model Controller/userDataModelController.swift
@@ -0,0 +1,9 @@
+//
+//  userDataModel.swift
+//  cruising-app
+//
+//  Created by Peter Luo on 3/16/19.
+//  Copyright © 2019 Christian Lim. All rights reserved.
+//
+
+import Foundation
---
+++ cruising-app/cruising-app/Model/eventModel.swift
@@ -0,0 +1,10 @@
+//
+//  eventModel.swift
+//  cruising-app
+//
+//  Created by Peter Luo on 3/16/19.
+//  Copyright © 2019 Christian Lim. All rights reserved.
+//
+
+import Foundation
+import Firebase
